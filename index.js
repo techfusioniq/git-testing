c4179d3b594e5a819d082213f82df2d318d59395
techfusioniq <vrtechie.web@gmail.com>
2024-10-23 11:28:52 +0530

    First Code Added

---
+++ index.js
@@ -0,0 +1,10 @@
+const age = 24;
+const name="vivan"
+const dob='04-10-2000'
+
+function SayName(name)
+{
+    console.log(`My Name is ${name}`)
+}
+
+SayName("yash")
---
+++ script.js
@@ -0,0 +1,1 @@
+const yourage='52'
