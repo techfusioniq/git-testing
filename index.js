7e59c7df24685b8ac2e350242ddce5b3c031d321
techFusionIQ <techfusioniq@gmail.com>
2024-10-24 10:55:21 +0530

    replace variable name

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const age = 24;
-const name="vivan"
+const name1="vivan"
 const dob='04-10-2000'
 
 function SayName(name)
 {
-    console.log(`My Name is ${name}`)
+    console.log(`My Name is ${name1}`)
 }
 
 SayName("yash")
